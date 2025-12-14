40010699ef2ddef40c837052641454a7d2e0c509
Gabriel Marin <marinjhow@gmail.com>
2025-12-14 12:45:22 -0300

    chore: Inclui a versao do DTO pela entidade Ninja que mapeia manualmente o NinjaMapper os caminhos entidade x dto

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissaoModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private String classe;
+    private int idade;
+    private MissaoModel missao;
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,34 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setClasse(ninjaDTO.getClasse());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setMissao(ninjaDTO.getMissao());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setClasse(ninjaModel.getClasse());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setMissao(ninjaModel.getMissao());
+
+        return ninjaDTO;
+    }
 }
