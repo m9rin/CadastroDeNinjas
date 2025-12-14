87b3a7c0bbef2739af71c50e125f5cdd141835d3
Gabriel Marin <marinjhow@gmail.com>
2025-12-14 12:46:43 -0300

    chore: inclui a versao do DTO pela entidade Missao que mapeia manualmente o MissaoMapper os caminhos entidade x dto

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoMapper.java
@@ -1,4 +1,27 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissaoMapper {
+
+    public MissaoModel map(MissaoDTO missaoDTO) {
+        MissaoModel missaoModel = new MissaoModel();
+        missaoModel.setId(missaoDTO.getId());
+        missaoModel.setNome(missaoDTO.getNome());
+        missaoModel.setRank(missaoDTO.getRank());
+        missaoModel.setNinjas(missaoDTO.getNinjas());
+
+        return missaoModel;
+    }
+
+    public MissaoDTO map(MissaoModel missaoModel) {
+        MissaoDTO missaoDTO = new MissaoDTO();
+        missaoDTO.setId(missaoModel.getId());
+        missaoDTO.setNome(missaoModel.getNome());
+        missaoDTO.setRank(missaoModel.getRank());
+        missaoDTO.setNinjas(missaoModel.getNinjas());
+
+        return missaoDTO;
+    }
 }
